27c1424abf757155a67bc99e03b282856e2755b4
Erick <erick_tec@outlook.com>
2022-01-10 19:17:16 -0600

    initial commit

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef } from 'react';
+import { ReactComponent as Wave } from "./wave.svg";
+import "./Timers.scss";
+
+const Timer = ({ time }) => {
+    const [timeRemaining, setTimeRemaining] = useState(time);
+    const wave = useRef();
+    if (wave.current) wave.current.style.top = `${100 - (timeRemaining / time * 100)}%`
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeRemaining(timeRemaining => timeRemaining - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+
+    return (
+        <div className='timer'>
+            <div ref={wave} className="timer__wave"></div>
+            <p className="timer__seconds">{timeRemaining}</p>
+        </div>
+    )
+}
+
+Timer.defaultProps = {
+    time: 60
+}
+
+export default Timer;
